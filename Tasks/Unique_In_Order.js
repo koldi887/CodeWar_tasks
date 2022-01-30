3a3c1f4098a2c34a8e994be0107b7024d0ef73c3
dmitri_kolisnitsenko <koldi887@gmail.com>
2022-01-30 12:06:04 +0200

    unique

---
+++ Tasks/Unique_In_Order.js
@@ -0,0 +1,9 @@
+function uniqueInOrder(iterable){
+    let arr = []
+    for (let i = 0; i < iterable.length; i++){
+        if(iterable[i] != iterable[i+1]) {
+            arr.push(iterable[i])
+        }
+    }
+    return arr
+}
